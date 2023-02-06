670331d3744a3013eef833f7eece0292ddf52401
Devin Riley <devin.riley144@gmail.com>
2023-02-06 10:43:32 -0600

    added 'create new' section

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -9,6 +11,6 @@
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
 
-
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
